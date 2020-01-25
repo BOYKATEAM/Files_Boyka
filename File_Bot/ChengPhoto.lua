e6214e0e1160d12d316fef334d2ad91f088cb395
BOYKABOT <58709598+BOYKABOT@users.noreply.github.com>
2020-01-26 02:35:52 +0300

    Add files via upload

---
+++ File_Bot/ChengPhoto.lua
@@ -0,0 +1,35 @@
+
+local function ChengPhoto(msg)
+local text = msg.content_.text_
+if text then 
+tdcli_function({ID = "GetUser",user_id_ = msg.sender_user_id_},function(arg,data)
+if data.id_ then 
+if data.id_ ~= bot_id then 
+local ChengPhoto = database:get(bot_id..'Cheng:Photo'..data.id_)
+if not data.profile_photo_ then 
+if ChengPhoto then 
+send(msg.chat_id_, msg.id_, "حذف كل صوره الحلو 😂👌🏻")
+database:del(bot_id..'Cheng:Photo'..data.id_) 
+end
+end
+if data.profile_photo_.big_.persistent_id_ then 
+if TshakeChengPhoto ~= data.profile_photo_.big_.persistent_id_ then 
+local Text = {
+  "شكو غيرت صورتك  يا حلو ",
+  "منور طالع حلو عل صوره جديده",
+  "ها شو غيرت صورتك 🤔😹",
+  "شكو غيرت صورتك شنو قطيت وحده جديده 😹😹🌚",
+  "شو غيرت صورتك شنو تعاركت ويه الحب ؟😹🌞",
+  "شكو غيرت الصوره شسالفه ؟؟ 🤔🌞",
+}
+send(msg.chat_id_, msg.id_,Text[math.random(#Text)])
+end  
+database:set(bot_id..'Cheng:Photo'..data.id_, data.profile_photo_.big_.persistent_id_) 
+end
+end
+end
+end,nil)   
+end
+
+end
+return {Poyka = ChengPhoto}
