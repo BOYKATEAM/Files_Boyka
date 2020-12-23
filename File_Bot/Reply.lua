adf8e0a3c8f38e895d7468f9750c1abe60633bf2
BOYKATEAM <61522730+BOYKATEAM@users.noreply.github.com>
2020-12-23 18:24:02 +0300

    Add files via upload

--- File_Bot/Reply.lua
+++ File_Bot/Reply.lua
@@ -290,13 +290,13 @@ end
 
 if text == 'تفعيل ردود البوت' and Manager(msg) then
 database:del(bot_id..'Reply:Status'..msg.chat_id_)
-send(msg.chat_id_, msg.id_,'✸∫ تم تفعيل ردود البوت')
+send(msg.chat_id_, msg.id_,'❃∫ تم تفعيل ردود البوت')
 return false
 end
 
 if text == 'تعطيل ردود البوت' and Manager(msg) then
 database:set(bot_id..'Reply:Status'..msg.chat_id_,true)
-send(msg.chat_id_, msg.id_,'✸∫ تم تعطيل ردود البوت')
+send(msg.chat_id_, msg.id_,'❃∫ تم تعطيل ردود البوت')
 return false
 end
 
